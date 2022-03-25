cb79d38e9af69ddfa42fe49452f6642a25789d0a
Yoni Kosiner <yoni@kosiner.co.uk>
2022-03-25 09:22:22 +0000

    Clean up the code a bit

--- lua/todo-me-daddy/init.lua
+++ lua/todo-me-daddy/init.lua
@@ -20,136 +20,15 @@ function os.capture(cmd, raw)
   return s
 end
 
-function files_from_dir(dir)
-    -- Find each file in the current dir, sorry windows users this only works on posix complaint shells (fuck you windows)
-    local files = os.capture("find " .. dir)
-
-    -- Get each item, split by new line, and insert it into the table fileTable
-    local s = " "
-    for f in string.gmatch(files, "([^" .. s .. "]+)") do
-        -- Get each file from the ignore list, you can find info on this in the
-        -- README (by default the progarm ignores node_modules, and right now
-        -- if you change that 0 to do's wil be found)
-
-        for k,v in pairs(methods.Get('ignore_folders')) do
-            if string.find(f, v) then
-                break
-            else
-                table.insert(fileTable, f)
-            end
-        end
-        -- error("Go back and read the readme, smh. Hint it is the ignore_folders option")
-    end
-end
-
-function lines_from(file)
-    local lines = {}
-    -- Read each file and split by new line, and insert it into the table fileTable (include line numbers)
-    for line in io.lines(file) do
-        local lineNum = #lines + 1
-        table.insert(lines, lineNum .. line)
-    end
-    return lines
-end
-
 local M = {
     setup = methods.Setup,
 }
 
-function get_todo_comments()
-    -- Each time we run this, we need to clear the table
-    -- This is because we are running this function multiple times
-    -- and we don't want to add the same comments multiple times
-    local todos = {}
-
-    for k,v in pairs(fileTable) do
-        local file = v
-        -- Make sure that the file is not a dir
-        if utils:file_not_dir(file) then
-            local lines = lines_from(file)
-            for k,v in pairs(lines) do
-                v = string.gsub(v, "^%s*", "")
-                v = string.gsub(v, "^(%d+)", "%1 ")
-                v = string.gsub(v, "^(%d+)%s*", "%1 ")
-
-                local stringWithNoNumber = string.gsub(v, "^(%d+)%s*", "")
-                stringWithNoNumber = string.gsub(stringWithNoNumber, "^%s*", "")
-                local filetype = string.match(file, "%.([^.]+)$")
-
-                if not methods.Get("get_markdown_todo") == false then
-                    if filetype == "md" then
-                        -- Oh god regex is so ugly
-                        -- Lua uses % not \ for escaping
-                        if string.find(stringWithNoNumber, "^-%s%[ ]") then
-                            local todoComment = "%s %s"
-                            v = string.format(todoComment, v, file)
-                            table.insert(todos, v)
-                        end
-                    end
-                end
-                --TODO: Clean this up, it's ugly as hell
-                if string.find(v, "TODO") then
-                    --TODO: implent this
-                    -- local commentString = vim.cmd("echo &commentstring")
-
-                    -- if commentString == "--" then
-                    --     local todoComment = "-- %s %s"
-                    --     v = string.format(todoComment, v, file)
-                    --     table.insert(todos, v)
-                    -- else
-                    --     local todoComment = "%s %s"
-                    --     v = string.format(todoComment, v, file)
-                    --     table.insert(todos, v)
-                    -- end
-
-                    if filetype == "lua" then
-                        if string.find(stringWithNoNumber, "^--TODO") or string.find(stringWithNoNumber, "^-- TODO") then
-                            local todoComment = "%s %s"
-                            v = string.format(todoComment, v, file)
-                            table.insert(todos, v)
-                        end
-                    elseif filetype == "js" or filetype == "ts" or filetype == "go" or filetype == "c" then
-                        if string.find(stringWithNoNumber, "^//TODO") or string.find(stringWithNoNumber, "^// TODO") then
-                            local todoComment = "%s %s"
-                            v = string.format(todoComment, v, file)
-                            table.insert(todos, v)
-                        end
-                    elseif filetype == "sh" or filetype == "py" then
-                        if string.find(stringWithNoNumber, "^#TODO")  or string.find(stringWithNoNumber, "^# TODO") then
-                            local todoComment = "%s %s"
-                            v = string.format(todoComment, v, file)
-                            table.insert(todos, v)
-                        end
-                    elseif filetype == "" or filetype == nil then
-                        if string.find(stringWithNoNumber, "^#TODO") or string.find(stringWithNoNumber, "^# TODO") then
-                            local todoComment = "%s %s"
-                            v = string.format(todoComment, v, file)
-                            table.insert(todos, v)
-                        end
-                    else
-                        if string.find(stringWithNoNumber, "^//TODO") or string.find(stringWithNoNumber, "^// TODO") then
-                            local todoComment = "%s %s"
-                            v = string.format(todoComment, v, file)
-                            table.insert(todos, v)
-                        end
-                    end
-                end
-            end
-        end
-    end
-
-    return todos
-end
-
-function get_current_dir()
-    return vim.fn.getcwd()
-end
-
 function get_todos()
-    fileTable = {}
-    local currentDir = get_current_dir()
-    files_from_dir(currentDir)
-    return get_todo_comments()
+    files.fileTable = {}
+    local currentDir = utils:get_current_dir()
+    files:files_from_dir(currentDir)
+    return utils:get_todo_comments()
 end
 
 M.get_todo_github = function()
--- lua/todo-me-daddy/utils.lua
+++ lua/todo-me-daddy/utils.lua
@@ -1,4 +1,81 @@
+local files = require("todo-me-daddy.files")
+local methods = require("todo-me-daddy.methods")
 local utils = {}
+
+function utils:get_todo_comments()
+    -- Each time we run this, we need to clear the table
+    -- This is because we are running this function multiple times
+    -- and we don't want to add the same comments multiple times
+    local todos = {}
+
+    for k,v in pairs(files.fileTable) do
+        local file = v
+        -- Make sure that the file is not a dir
+        if utils:file_not_dir(file) then
+            local lines = files:get_line_from_file(file)
+            for k,v in pairs(lines) do
+                v = string.gsub(v, "^%s*", "")
+                v = string.gsub(v, "^(%d+)", "%1 ")
+                v = string.gsub(v, "^(%d+)%s*", "%1 ")
+
+                local stringWithNoNumber = string.gsub(v, "^(%d+)%s*", "")
+                stringWithNoNumber = string.gsub(stringWithNoNumber, "^%s*", "")
+                local filetype = string.match(file, "%.([^.]+)$")
+
+                if not methods.Get("get_markdown_todo") == false then
+                    if filetype == "md" then
+                        -- Oh god regex is so ugly
+                        -- Lua uses % not \ for escaping
+                        if string.find(stringWithNoNumber, "^-%s%[ ]") then
+                            local todoComment = "%s %s"
+                            v = string.format(todoComment, v, file)
+                            table.insert(todos, v)
+                        end
+                    end
+                end
+                --TODO: Clean this up, it's ugly as hell
+                if string.find(v, "TODO") then
+                    --TODO: implent this
+                    -- local commentString = vim.cmd("echo &commentstring")
+
+                    if filetype == "lua" then
+                        if string.find(stringWithNoNumber, "^--TODO") or string.find(stringWithNoNumber, "^-- TODO") then
+                            local todoComment = "%s %s"
+                            v = string.format(todoComment, v, file)
+                            table.insert(todos, v)
+                        end
+                    elseif filetype == "js" or filetype == "ts" or filetype == "go" or filetype == "c" then
+                        if string.find(stringWithNoNumber, "^//TODO") or string.find(stringWithNoNumber, "^// TODO") then
+                            local todoComment = "%s %s"
+                            v = string.format(todoComment, v, file)
+                            table.insert(todos, v)
+                        end
+                    elseif filetype == "sh" or filetype == "py" then
+                        if string.find(stringWithNoNumber, "^#TODO")  or string.find(stringWithNoNumber, "^# TODO") then
+                            local todoComment = "%s %s"
+                            v = string.format(todoComment, v, file)
+                            table.insert(todos, v)
+                        end
+                    elseif filetype == "" or filetype == nil then
+                        if string.find(stringWithNoNumber, "^#TODO") or string.find(stringWithNoNumber, "^# TODO") then
+                            local todoComment = "%s %s"
+                            v = string.format(todoComment, v, file)
+                            table.insert(todos, v)
+                        end
+                    else
+                        if string.find(stringWithNoNumber, "^//TODO") or string.find(stringWithNoNumber, "^// TODO") then
+                            local todoComment = "%s %s"
+                            v = string.format(todoComment, v, file)
+                            table.insert(todos, v)
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    return todos
+end
 
 function utils:exists(file)
    local ok, err, code = os.rename(file, file)
@@ -17,6 +94,10 @@ function utils:file_not_dir(name)
     else
         return true
     end
+end
+
+function utils:get_current_dir()
+    return vim.fn.getcwd()
 end
 
 -- TODO: use this bad boy instead of the scuffed way of doing it
